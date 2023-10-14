985837a04d72f4a995744b5a2e5d6e9bd77b7b7f
Sindhurae <146705822+Sindhurae@users.noreply.github.com>
2023-10-14 16:07:30 +0530

    Add files via upload

---
+++ day1/full_adder_tb.v
@@ -0,0 +1,14 @@
+module full_adder_tb;
+reg A,B,C;
+wire S,Cout;
+full_adder f1(S,Cout,A,B,C);
+initial 
+begin
+$dumpfile("full_adder.vcd");
+$dumpvars(0,full_adder_tb);
+$monitor("%0t,S=%b,Cout=%b,A=%b,B=%b,C=%b",$time,S,Cout,A,B,C);
+A=1;B=0;C=0;
+#5 A=0;B=1;C=1;
+#5 A=0;B=1;C=0;
+end
+endmodule
