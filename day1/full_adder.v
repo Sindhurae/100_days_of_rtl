d46676d8d0ad599133d72a95ba232142ee660c60
Sindhurae <146705822+Sindhurae@users.noreply.github.com>
2023-10-14 16:29:02 +0530

    Indendation

--- day1/full_adder.v
+++ day1/full_adder.v
@@ -1,6 +1,6 @@
 module full_adder(S,Cout,A,B,C);
-input A,B,C;
-output S,Cout;
-assign S=A^B^C;
-assign Cout=(A&B)|(B&C)|(A&C);
+    input A,B,C;
+    output S,Cout;
+    assign S=A^B^C;
+    assign Cout=(A&B)|(B&C)|(A&C);
 endmodule
